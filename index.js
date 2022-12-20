df0d1551e1720dd3df626c3f3c4fa450cc17bd80
Abyanzhafran <zhafranjupleks5758@gmail.com>
2022-12-20 14:42:35 +0700

    update : - add module_test folder - read root folder and files,  - config for custom specific folder - still not working for read all file in nested folder

--- index.js
+++ index.js
@@ -1,18 +1,39 @@
 const fs = require('fs');
 const path = require('path')
 
+// Read root file
+fs.readdir('./', (err, files) => {
+  const rootFiles = []
+
+  files.forEach(filename => {
+    const ext = path.parse(filename).ext
+    const filepath = path.resolve(__dirname, filename)
+    const stat = fs.statSync(filepath)
+    const isFile = stat.isFile()
+    // console.log(isFile)
+    if (isFile == true && ext == '.js') {
+      const content = fs.readFileSync(filepath, 'utf-8')
+      rootFiles.push({ content })
+    }
+  });
+
+  return rootFiles
+});
+
 function readFilesSync(dir) {
-  const files = [];
+  const files = []
+  // console.log(dir)
 
   fs.readdirSync(dir).forEach(filename => {
-    const name = path.parse(filename).name;
-    const ext = path.parse(filename).ext;
-    const filepath = path.resolve(dir, filename);
-    const stat = fs.statSync(filepath);
-    const isFile = stat.isFile();
+    const name = path.parse(filename).name
+    const ext = path.parse(filename).ext
+    const filepath = path.resolve(dir, filename)
+    const stat = fs.statSync(filepath)
+    const isFile = stat.isFile()
     const content = fs.readFileSync(filepath, 'utf-8')
 
-    if (isFile) files.push({ filepath, name, ext, content });
+    if (isFile) files.push({ dir, filepath, name, ext, content })
+
   });
 
   files.sort((a, b) => {
@@ -21,9 +42,13 @@ function readFilesSync(dir) {
     return a.name.localeCompare(b.name, undefined, { numeric: true, sensitivity: 'base' });
   });
 
-  // return files;
-  console.log(files)
+  return files;
 }
 
-const files = readFilesSync('src/');
+// Add your path to check the package
+// By default its check "src" path and "root directory"
+readFilesSync('src/')
+readFilesSync('node_modules_test/')
+
+
 
