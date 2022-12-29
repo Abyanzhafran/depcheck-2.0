a0cf52d9bfa769756a6970450311bed78cee0fb6
Abyanzhafran <zhafranjupleks5758@gmail.com>
2022-12-29 14:52:42 +0700

    remove : readFilesSync function

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ function getFileContentFromSpecificDir(dir) {
 
   if (dir != undefined) {
     fs.readdirSync(dir).forEach(filename => {
-      const name = path.parse(filename).name
       const ext = path.parse(filename).ext
       const filepath = path.resolve(dir, filename)
       const stat = fs.statSync(filepath)
@@ -28,31 +27,7 @@ function getFileContentFromSpecificDir(dir) {
   }
 }
 
-function readFilesSync(dir) {
-  const files = []
-
-  fs.readdirSync(dir).forEach(filename => {
-    const name = path.parse(filename).name
-    const ext = path.parse(filename).ext
-    const filepath = path.resolve(dir, filename)
-    const stat = fs.statSync(filepath)
-    const isFile = stat.isFile()
-    const content = fs.readFileSync(filepath, 'utf-8')
-
-    if (isFile) files.push({ dir, filepath, name, ext, content })
-  });
-
-  files.sort((a, b) => {
-    // natural sort alphanumeric strings
-    // https://stackoverflow.com/a/38641281
-    return a.name.localeCompare(b.name, undefined, { numeric: true, sensitivity: 'base' });
-  });
-
-  return files;
-}
-
 module.exports = {
-  getFileContentFromSpecificDir,
-  readFilesSync
+  getFileContentFromSpecificDir
 }
 
