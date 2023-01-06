53bdcc463fb2abe36da337b1f51f7e4dab6c58a1
Abyanzhafran <zhafranjupleks5758@gmail.com>
2023-01-06 14:44:27 +0700

    feat : - app can detect js file in directory and it sub-directory - app can get all file content to search used js package - tidy up code in index.js file

--- index.js
+++ index.js
@@ -3,20 +3,18 @@ const path = require('path');
 const { kmpSearch } = require('./src/function-process/kmp')
 const { getFileContentFromSpecificDir } = require('./src/function-process/getFileContent')
 
-// Read package.json file
 const packageJson = JSON.parse(fs.readFileSync(path.join(__dirname, 'package-test-1.json'), 'utf8'));
-
-// Get list of dependencies from package.json
 const dependencies = new Set(Object.keys(packageJson.dependencies));
 
-// Read all javaScript files in the project
 const files = []
-files.push(getFileContentFromSpecificDir('./src/file-test'))
+
+// don't change this line, default config
+files.push(getFileContentFromSpecificDir('./src'))
 files.push(getFileContentFromSpecificDir())
 
+// Iterate through files data stack to get the content
 for (const i in files) {
   for (const j of files[i]) {
-    // return j.content
     for (const deps of dependencies) {
       if (kmpSearch(deps, j.content)) {
         dependencies.delete(deps)
@@ -24,16 +22,6 @@ for (const i in files) {
     }
   }
 }
-
-// Original code
-// for (const file of files) {
-//   const content = fs.readFileSync(path.join(__dirname, file), 'utf8');
-//   for (const dependency of dependencies) {
-//     if (kmpSearch(dependency, content)) {
-//       dependencies.delete(dependency);
-//     }
-//   }
-// }
 
 // Print unused dependencies
 console.log(Array.from(dependencies));
--- src/test.js
+++ src/test.js
@@ -4,9 +4,6 @@ import App from "./App"
 import "bootstrap/dist/css/bootstrap.min.css"
 import { BudgetsProvider } from "./contexts/BudgetsContext"
 
-// new package
-// import Container from "react-bootstrap/Container"
-
 ReactDOM.render(
   <React.StrictMode>
     <BudgetsProvider>
