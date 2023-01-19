194aa38f5dbb3448ee39877fe45c6ca677150b6a
Abyanzhafran <zhafranjupleks5758@gmail.com>
2023-01-19 18:08:24 +0700

    feat: can read package.json script value, and check it as plain string with kmp algorithm

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ const dependencies = new Set(Object.keys(packageJson.dependencies));
 const files = []
 
 // don't change this line, default config
+const packageScript = Object.values(packageJson.scripts).toString();
 files.push(getFileContentFromSpecificDir('../../'))
+files[0].push({ filename: 'packageJsonScripts', content: packageScript })
 
 // Iterate through files data stack to get the content
 for (const i in files) {
