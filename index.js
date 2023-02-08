05963a7ba2a3dd511845926261db2cd49fb0cf89
Abyanzhafran <zhafranjupleks5758@gmail.com>
2023-02-08 11:53:48 +0700

    feat: improved function algorithm for searching all package.json file in a project

--- index.js
+++ index.js
@@ -1,44 +1,47 @@
-const fs = require('fs')
-const path = require('path')
 const { performance } = require('perf_hooks')
 const { kmpSearch } = require('./src/function-process/kmp')
-const { getFileContentFromSpecificDir } = require('./src/function-process/getFileContent')
+const {
+  getFileContentFromSpecificDir,
+  getPackageJsonDependencies
+} = require('./src/function-process/getFileContent')
 
 const start = performance.now()
 
-const packageJson = JSON.parse(fs.readFileSync(path.join('../../', 'package.json'), 'utf8'))
-const dependencies = packageJson.dependencies
-  ? new Set(Object.keys(packageJson.dependencies))
-  : new Set()
-const devDependencies = packageJson.devDependencies
-  ? new Set(Object.keys(packageJson.devDependencies))
-  : new Set()
-
 const files = []
+const packages = getPackageJsonDependencies('../../')
 
 // don't change this line, default config
 files.push(getFileContentFromSpecificDir('../../'))
 
+// merge deps and devDeps
+let mergedDeps = new Set()
+let mergedDevDeps = new Set()
+
+for (const i of packages) {
+  mergedDeps = new Set([...mergedDeps, ...Array.from(i.dependencies)])
+  mergedDevDeps = new Set([...mergedDeps, ...Array.from(i.devDependencies)])
+}
+
 // Iterate through files data stack to get the content
 for (const i in files) {
   for (const j of files[i]) {
-    for (const deps of dependencies) {
+    for (const deps of mergedDeps) {
       if (kmpSearch(deps, j.content)) {
-        dependencies.delete(deps)
+        mergedDeps.delete(deps)
       }
     }
 
-    for (const devDeps of devDependencies) {
+    for (const devDeps of mergedDevDeps) {
       if (kmpSearch(devDeps, j.content)) {
-        devDependencies.delete(devDeps)
+        mergedDevDeps.delete(devDeps)
       }
     }
   }
 }
 
 // Print unused dependencies
-console.log('Unused dependencies : ', Array.from(dependencies));
-console.log('Unused devDependecies : ', Array.from(devDependencies));
+console.log('Unused dependencies : ', Array.from(mergedDeps));
+console.log('Unused devDependencies : ', Array.from(mergedDevDeps).length);
 
 const end = performance.now();
 const executionTime = end - start;
--- src/function-process/getFileContent.js
+++ src/function-process/getFileContent.js
@@ -2,8 +2,8 @@ const fs = require('fs')
 const path = require('path')
 
 function getFileContentFromSpecificDir(dir) {
-  let fileContent = [];
-  const files = fs.readdirSync(dir);
+  let fileContent = []
+  const files = fs.readdirSync(dir)
 
   files.forEach(filename => {
     const filePath = path.join(dir, filename)
@@ -35,7 +35,39 @@ function getFileContentFromSpecificDir(dir) {
   return fileContent
 }
 
+function getPackageJsonDependencies(dir) {
+  let packageContent = []
+  const files = fs.readdirSync(dir)
+
+  files.forEach(filename => {
+    const filePath = path.join(dir, filename)
+    const stat = fs.statSync(filePath)
+
+    if (filename === "node_modules") {
+      return
+
+    } else if (filename === "package.json") {
+      const data = fs.readFileSync(path.join(dir, filename), 'utf-8')
+
+      packageDependencies = JSON.parse(data).dependencies
+        ? new Set(Object.keys(JSON.parse(data).dependencies))
+        : new Set()
+      packageDevDependencies = JSON.parse(data).devDependencies
+        ? new Set(Object.keys(JSON.parse(data).devDependencies))
+        : new Set()
+
+      packageContent.push({ dependencies: packageDependencies, devDependencies: packageDevDependencies })
+
+    } else if (stat.isDirectory()) {
+      packageContent = packageContent.concat(getPackageJsonDependencies(filePath))
+    }
+  })
+
+  return packageContent
+}
+
 module.exports = {
-  getFileContentFromSpecificDir
+  getFileContentFromSpecificDir,
+  getPackageJsonDependencies
 }
 
